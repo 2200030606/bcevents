f271743ebbc7e3ccf3c0ee50acf4b65d9bb5d783
2200030606 <2200030606@kluniversity.in>
2024-05-01 19:58:57 +0530

    changed client

--- src/client/ViewAllEvents.js
+++ src/client/ViewAllEvents.js
@@ -119,7 +119,7 @@ export default function ViewAllEvents(props) {
         {filteredEvents.map((event) => (
           <ImageListItem key={event.eventid} sx={{ borderBottom: '1px solid black' }}>
             <img
-              src={`${config.url}/clientevents/clienteventimage/${event.file}`}
+               src={`${config.url}/clientevents/clienteventimage/${event.file}`}
               style={{ width: '100%', height: '190%', objectFit: 'cover' }}
               alt={event.title}
               loading="lazy"
